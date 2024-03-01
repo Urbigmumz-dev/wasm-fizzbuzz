1c9a643b2ded9f8cf569139d13d8d57d831daefd
Urbigmumz <140040348+Urbigmumz-dev@users.noreply.github.com>
2024-03-01 16:01:35 +0000

    ye

--- index.mjs
+++ index.mjs
@@ -8,16 +8,6 @@ const PORT = 8080;
 app.use(express.static('doom/'));
 app.use(bodyParser.urlencoded({ extended: true, }));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', {
-        root: 'doom/',
-    }, (err) => {
-        if (err) {
-            console.error(err.message);
-        }
-    });
-});
-
 http.createServer(app).listen(PORT, (err) => {
     if (!err) {
         console.log('ur mom');
